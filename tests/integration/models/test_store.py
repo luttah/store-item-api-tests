4992909006400d1165d717821f7cec8270297ed2
charles <charles@arwvis.com>
2019-03-07 13:01:58 +0000

    Updated failing store tests

--- tests/integration/models/test_store.py
+++ tests/integration/models/test_store.py
@@ -41,6 +41,7 @@ class StoreTest(BaseTest):
     def test_store_json(self):
         store = StoreModel('test')
         expected = {
+            'id': None,
             'name': '',
             'items': []
         }
@@ -56,6 +57,7 @@ class StoreTest(BaseTest):
             item.save_to_db()
 
             expected = {
+                'id': 1,
                 'name': 'test',
                 'items': [{'name': 'test_item', 'price': 19.99}]
             }
