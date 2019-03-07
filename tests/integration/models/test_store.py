f38c42f789815d6ee598dfc70eca438ad888520a
charles <charles@arwvis.com>
2019-03-07 11:52:11 +0000

    Initial commit with of all tests including POSTMAN files

---
+++ tests/integration/models/test_store.py
@@ -0,0 +1,63 @@
+from models.item import ItemModel
+from models.store import StoreModel
+from tests.integration.integration_base_test import BaseTest
+
+
+class StoreTest(BaseTest):
+    def test_create_store_items_empty(self):
+        store = StoreModel('test')
+
+        self.assertListEqual(store.items.all(), [],
+                             "The store's items length was not 0 even though no items were added.")
+
+    def test_crud(self):
+        with self.app_context():
+            store = StoreModel('test')
+
+            self.assertIsNone(StoreModel.find_by_name('test'),
+                              "Found store with name test even though it was not written to the database")
+
+            store.save_to_db()
+
+            self.assertIsNotNone(StoreModel.find_by_name('test'),
+                                 "Did not find a store with the name test even though one was created in the database")
+
+            store.delete_from_db()
+
+            self.assertIsNone(StoreModel.find_by_name('test'),
+                              "Found store with name test even though it was deleted from the database")
+
+    def test_store_relationship(self):
+        with self.app_context():
+            store = StoreModel('test')
+            item = ItemModel('test_item', 19.99, 1)
+
+            store.save_to_db()
+            item.save_to_db()
+
+            self.assertEqual(store.items.count(), 1)
+            self.assertEqual(store.items.first().name, 'test_item')
+
+    def test_store_json(self):
+        store = StoreModel('test')
+        expected = {
+            'name': '',
+            'items': []
+        }
+
+        self.assertDictEqual(store.json(), expected)
+
+    def test_store_json_with_item(self):
+        with self.app_context():
+            store = StoreModel('test')
+            item = ItemModel('test_item', 19.99, 1)
+
+            store.save_to_db()
+            item.save_to_db()
+
+            expected = {
+                'name': 'test',
+                'items': [{'name': 'test_item', 'price': 19.99}]
+            }
+
+            self.assertDictEqual(store.json(), expected)
